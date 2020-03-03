6a3199d561b95b4503e8e9e550010a2b1e5461d4
Michael Redig <mredig@gmail.com>
2020-03-03 01:14:50 -0600

    (fix) updated package info

--- Package.swift
+++ Package.swift
@@ -5,6 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "LS8Swift",
+	products: [
+		// Products define the executables and libraries produced by a package, and make them visible to other packages.
+		.library(
+			name: "LS8Core",
+			targets: ["LS8Core"]),
+	],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
